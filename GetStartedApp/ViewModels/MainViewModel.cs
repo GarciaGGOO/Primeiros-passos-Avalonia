e158e317b5bc0bfd489aadccb654c5f87bd288c9
Guilherme Garcia <guilherme.garciaortega@gmail.com>
2024-06-12 13:07:19 -0300

    first commit

---
+++ GetStartedApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace GetStartedApp.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Tentando fazer u projeto avalonia";
+}
---
+++ GetStartedApp/Views/MainView.axaml.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using Avalonia.Interactivity;
+using System.Diagnostics;
+using System;
+using Avalonia;
+
+namespace GetStartedApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        ConvertCelsiusToFahrenheit();
+    }
+
+    private void ConvertCelsiusToFahrenheit()
+    {
+        if (Double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
+
+        Debug.WriteLine($"Click! Celsius={celsius.Text}, Fahrenheit={fahrenheit.Text}");
+    }
+
+    private void Celsius_TextChanged(object? sender, Avalonia.Controls.TextChangedEventArgs e)
+    {
+        ConvertCelsiusToFahrenheit();
+    }
+}
